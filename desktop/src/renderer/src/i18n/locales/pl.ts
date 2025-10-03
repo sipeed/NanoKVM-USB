24e5091e3f6edf5b971e608fad7070829d16e789
Sebastian Brzuzek <sebastian@brzuzek.pl>
2025-10-03 21:04:30 +0200

    Polish language translation

---
+++ desktop/src/renderer/src/i18n/locales/pl.ts
@@ -0,0 +1,84 @@
+const pl = {
+  translation: {
+    camera: {
+      tip: 'Oczekiwanie na autoryzację...',
+      denied: 'Brak uprawnień',
+      authorize:
+        'Zdalny pulpit wymaga uprawnienia dostępu do kamery. Zezwól na dostęp do kamery w ustawieniach.',
+      failed: 'Nie udało połączyć się z kamerą. Spróbuj ponownie.'
+    },
+    modal: {
+      title: 'Wybierz urządzenie USB',
+      selectVideo: 'Wybierz urządzenie wejścia wideo',
+      selectSerial: 'Wybierz urządzenie portu szeregowego'
+    },
+    menu: {
+      serial: 'Port szeregowy',
+      keyboard: 'Klawiatura',
+      mouse: 'Mysz'
+    },
+    video: {
+      resolution: 'Rozdzielczość',
+      customResolution: 'Niestandardowa',
+      device: 'Urządzenie',
+      custom: {
+        title: 'Niestandardowa rozdzielczość',
+        width: 'Szerokość',
+        height: 'Wysokość',
+        confirm: 'Ok',
+        cancel: 'Anuluj'
+      }
+    },
+    keyboard: {
+      paste: 'Wklej',
+      virtualKeyboard: 'Klawiatura',
+      ctrlAltDel: 'Ctrl + Alt + Delete'
+    },
+    mouse: {
+      cursor: {
+        title: 'Kursor',
+        pointer: 'Wskaźnik',
+        grab: 'Chwyć',
+        cell: 'Zaznacz',
+        hide: 'Ukryj'
+      },
+      mode: 'Tryb myszy',
+      absolute: 'Tryb absolutny',
+      relative: 'Tryb względny',
+      direction: 'Kierunek kółka przewijania',
+      scrollUp: 'Przewijanie w górę',
+      scrollDown: 'Przewijanie w dół',
+      speed: 'Szybkość kółka',
+      fast: 'Szybko',
+      slow: 'Powoli',
+      requestPointer:
+        'Użycie trybu względnego. Proszę kliknij na pulpicie aby aktywować wskaźnik myszy.'
+    },
+    settings: {
+      title: 'Ustawienia',
+      appearance: {
+        title: 'Wygląd',
+        language: 'Język',
+        menu: 'Menu',
+        menuTips: 'Rozwiń menu przy starcie'
+      },
+      update: {
+        title: 'Sprawdź dostępność aktualizacji',
+        latest: 'Używana wersja jest najnowsza.',
+        outdated: 'Aktualizacja jest dostępna. Czy chcesz teraz wykonać aktualizację?',
+        downloading: 'Pobieranie...',
+        installing: 'Instalacja...',
+        failed: 'Aktualizacja nieudana. Spróbuj ponownie.',
+        confirm: 'Potwierdź',
+        cancel: 'Anuluj'
+      },
+      about: {
+        title: 'O aplikacji',
+        version: 'Wersja',
+        community: 'Społeczność'
+      }
+    }
+  }
+}
+
+export default pl
