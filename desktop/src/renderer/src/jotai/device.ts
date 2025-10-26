fc057dfb7ad5e82ac184f5fd6e9eda5f33b3eae7
James Brown <randomvoidmail@foxmail.com>
2025-10-27 02:33:53 +0800

    Add serial baud rate atom to device.ts

--- desktop/src/renderer/src/jotai/device.ts
+++ desktop/src/renderer/src/jotai/device.ts
@@ -4,6 +4,36 @@ import { Resolution } from '@renderer/types'
 
 type VideoState = 'disconnected' | 'connecting' | 'connected'
 type SerialState = 'notSupported' | 'disconnected' | 'connecting' | 'connected'
+type BaudRate = 
+  | "75"
+  | "110" 
+  | "134.5"
+  | "150"
+  | "300"
+  | "600"
+  | "1200"
+  | "1800"
+  | "2400"
+  | "4800"
+  | "7200"
+  | "9600"
+  | "14400"
+  | "19200"
+  | "28800"
+  | "31250"
+  | "38400"
+  | "57600"
+  | "74880"
+  | "115200"
+  | "230400"
+  | "250000"
+  | "460800"
+  | "500000"
+  | "921600"
+  | "1000000"
+  | "2000000"
+  | "3000000"
+  | "4000000"
 
 export const resolutionAtom = atom<Resolution>({
   width: 1920,
@@ -15,3 +45,4 @@ export const videoStateAtom = atom<VideoState>('disconnected')
 
 export const serialPortAtom = atom('')
 export const serialPortStateAtom = atom<SerialState>('disconnected')
+export const serialBaudRate = atom('57600')
